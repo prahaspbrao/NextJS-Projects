6f769e637a3b22b07d3272130522080d2022f178
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-09-19 00:19:25 +0530

    Added new variant to button.tsx file in shadcn

--- vibe-editor/components/ui/button.tsx
+++ vibe-editor/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        brand : "bg-brand-primary text-white p-4 rounded"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
