8d71dd8c7f992c56fd6acb0040f95a90e4fc6b80
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-09-19 00:14:54 +0530

    Added layout with grid and header , footer

--- vibe-editor/app/(root)/layout.tsx
+++ vibe-editor/app/(root)/layout.tsx
@@ -1,30 +1,41 @@
-import { Metadata } from "next"
-import {Header} from "@/modules/home/header"
-import Footer from "@/modules/home/footer"
+import { Metadata } from "next";
+import { Header } from "@/modules/home/header";
+import Footer from "@/modules/home/footer";
 
-
-export const metadata : Metadata = {
-    title : {
-        template : "VibeCode - Editor",
-        default : "Code Editor for VibeCoders - VibeCode"
-    }
-}
+export const metadata: Metadata = {
+  title: {
+    template: "VibeCode - Editor",
+    default: "Code Editor for VibeCoders - VibeCode",
+  },
+};
 
 export default function HomeLayout({
-    children
-} : {
-    children : React.ReactNode
-}){
-    return (
-        <>
-        <Header />
-        <main className="z-20 relative w-full pt-0 ">
-        {
-            children
-        }
-        </main>
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
 
-        <Footer />
-        </>
-    )
+      <div
+  className="
+    min-h-screen 
+    bg-white dark:bg-black 
+    [background-image:linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)] 
+    dark:[background-image:linear-gradient(to_right,rgba(255,255,255,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.08)_1px,transparent_1px)]
+    [background-size:40px_40px]
+  "
+>
+  <h1 className="p-10 text-4xl font-bold text-black dark:text-white">
+    Tailwind Grid Background
+  </h1>
+</div>
+
+
+      <main className="z-20 relative w-full pt-0 ">{children}</main>
+
+      <Footer />
+    </>
+  );
 }
--- vibe-editor/app/layout.tsx
+++ vibe-editor/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
 
   return (
     <SessionProvider session={session}>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
