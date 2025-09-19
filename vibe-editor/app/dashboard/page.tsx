9f4be8f69089646f680881ea4e35a7b3c771827a
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-09-20 00:48:37 +0530

    removed the extra files

--- vibe-editor/app/dashboard/page.tsx
+++ vibe-editor/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
+
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col justify-start items-center min-h-screen mx-auto px-4 py-10'>
+      <div className='grid grid-col-1 md:grid-cols-2 gap-6 w-full '>
+        <AddNewButton />
+        <AddRepo />
+      </div>
     </div>
   )
 }
